307658861f75a8551cbe0c4079efd1e8dfa8d3ee
Max Kornyev <mkornyev@Maxs-MBP.lan>
2025-11-19 18:47:23 -0500

    Base Application: Vite + React + Typescript + SVC

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-center text-3xl'>
+        Blacksmith Dash
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    prefix: 'tw-',
+    content: [
+      "./index.html",
+      "./src/**/*.{ts,tsx,js,jsx}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+}
+  
